04532d06a0c5df730f599e1ee1e818a4b565be9a
Napong <69472861+1napong1@users.noreply.github.com>
2022-10-19 19:16:52 +0700

    Add files via upload

---
+++ lsp/Square.java
@@ -0,0 +1,15 @@
+package lsp;
+
+public class Square extends Rectangle {
+    private int side; 
+    Square(int side) {
+        this.setSide(side);
+    }
+    public void setSide(int side) {
+    	this.side = side; 
+    }
+    @Override 
+    public int getArea(){ 
+        return side * side;
+    }
+}
